f00a46efb75d03ffb10099becfe0e112c0784891
shadowninja024 <63149314+shadowninja024@users.noreply.github.com>
2020-07-17 18:48:56 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,8 +3,7 @@ class Translation:
 Made with ❣️ by @Shadow_ninja_legit"""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "This bot can download any YouTube video and direct download link and upload to telegram 
-Made with ❣️ by @Shadow_ninja_legit "
+    UPGRADE_TEXT = "useless command "
     FORMAT_SELECTION = "Select the desired format: <a href='{}'>file size might be approximate</a> \nIf you want to set custom thumbnail, send photo before or quickly after tapping on any of the below buttons.\nYou can use /deletethumbnail to delete the auto-generated thumbnail."
     SET_CUSTOM_USERNAME_PASSWORD = """If you want to download premium videos, provide in the following format:
 URL | filename | username | password"""
